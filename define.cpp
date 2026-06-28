4932933fc88576dde4f8486f61dd5cfa66b38fdf
Kevin-PC <106233322+ungatonigro@users.noreply.github.com>
2026-06-28 14:58:52 +0800

    save the descriptions

--- define.cpp
+++ define.cpp
@@ -16,11 +16,6 @@ void recursion(node* n) {
     std::cout << n->id;
 }
 
-// bool edge::operator<(const edge& other) const {
-//         // 如果您希望權重小的先出列（最小堆），這裡要寫成 > 
-//         return this->weight < other.weight; 
-//     }
-
 Graph::Graph(int n) {
     nodes.resize(n);
     for (int i = 0; i < n; i++) {
@@ -56,25 +51,6 @@ void Graph::printRoute(int start, int end){
     recursion(temp); 
     std::cout << std::endl << "Path length: " << temp->distance << std::endl; 
 }
-
-// void Graph::dijkstra(int start) {
-//     nodes[start].distance = 0;
-//     std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
-//     pq.push(std::make_pair(0, start));
-//     while (!pq.empty()) {
-//         int u = pq.top().second;
-//         pq.pop();
-//         while (!nodes[u].edges.empty()) {
-//             edge e = nodes[u].edges.top();
-//             nodes[u].edges.pop();
-//             if (nodes[u].distance + e.weight < nodes[e.to].distance) {
-//                 nodes[u].next = &nodes[e.to];
-//                 nodes[e.to].distance = nodes[u].distance + e.weight;
-//                 pq.push(std::make_pair(nodes[e.to].distance, e.to));
-//             }
-//         }
-//     }
-// }
 
 // 假設 node 結構中是：std::vector<edge> edges;
 void Graph::dijkstra(int start) {
